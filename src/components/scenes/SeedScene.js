f5b75fdf005f932f3bcf0f03e2fae0881c9ba81d
Ramla Ijaz <ijazramla@gmail.com>
2023-05-08 12:03:47 -0400

    underground + mass/velocity for cubes + figure starts at a higher y

--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -7,6 +7,7 @@ import Cube from '../objects/Cube/Cube';
 import Ground from '../objects/Ground/Ground';
 import Sphere from '../objects/Sphere/Sphere';
 import Rectangle from '../objects/Background/Rectangle';
+import Plane from '../objects/Plane/Plane';
 
 
 class SeedScene extends Scene {
@@ -63,7 +64,7 @@ class SeedScene extends Scene {
         this.state.gui.add(this.state, 'Lives');
 
         // Set background to a nice color
-        this.background = new Color(0x7ec0ee);
+        this.background = new Color(0x7ec0ee); //sky = 0x87CEEB
 
         // Initialize the world for the physics engine
         this.world = new CANNON.World({
@@ -87,10 +88,15 @@ class SeedScene extends Scene {
         this.add(this.stick);
         this.world.addBody(this.stick.body);
 
+        // Add plane under ground
+        this.underground = new Plane();
+        this.add(this.underground);
+        
         // Add ground
         this.ground = new Ground(groundMaterial);
         this.add(this.ground);
         this.world.addBody(this.ground.body);
+
 
         // Add world boundaries
         const boundaryAngles = [Math.PI / 2, -Math.PI / 2, 0, Math.PI];
@@ -119,7 +125,7 @@ class SeedScene extends Scene {
             Math.random() * (this.playableArea.maxX - this.playableArea.minX) + this.playableArea.minX,
             this.generateHeight,
             Math.random() * (this.playableArea.maxZ - this.playableArea.minZ) + this.playableArea.minZ
-        ), 3 + Math.random ());
+        ), 3 + Math.random(), Math.random() * 20, Math.random() * 30);
 
         // Add some extra properties for the object
         newCube.body.hasCollidedWithGround = false;
